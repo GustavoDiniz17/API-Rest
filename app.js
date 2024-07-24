13f9c98e3a5a45d32c28c006181b9d5af33930fa
GustavoDiniz17 <gustavodinizdemorais@gmail.com>
2024-07-24 15:12:28 -0300

    Creation of Routes and Adjustments

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-const rotaProdutos = require('./routes/produtos');
-const rotaPedidos = require('./routes/pedidos');
+const rotaProdutos = require("./routes/produtos");
+const rotaPedidos = require("./routes/pedidos");
 
-app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos);
+app.use(morgan("dev"));
+
+app.use("/produtos", rotaProdutos);
+app.use("/pedidos", rotaPedidos);
+
+app.use((req, res, next) => {
+  const erro = new Error("Não Encontrado");
+  erro.status =404 ;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
