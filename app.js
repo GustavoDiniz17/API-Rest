e6ef118d51057f7fc0d917dbe28f6ab3a3748f0d
GustavoDiniz17 <gustavodinizdemorais@gmail.com>
2024-07-24 15:48:19 -0300

    Defining the Body and treating CORS

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).send({});
+  }
+
+  next();
+});
 
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
 
 app.use((req, res, next) => {
   const erro = new Error("Não Encontrado");
-  erro.status =404 ;
+  erro.status = 404;
   next(erro);
 });
 
