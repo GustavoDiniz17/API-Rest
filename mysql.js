9f7bda5b739836498956ac5b5a9b3f41aa14192f
GustavoDiniz17 <gustavodinizdemorais@gmal.com>
2024-08-17 00:29:49 -0300

    deploy

--- mysql.js
+++ mysql.js
@@ -1,13 +1,24 @@
-require('dotenv').config();
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT
-  });
+var pool = mysql.createPool({
+    "connectionLimit" : 1000,
+    "user" : process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database" : process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port" : process.env.MYSQL_PORT
+});
 
-  exports.pool = pool;
-  
+exports.execute = (query, params=[]) => {
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (error, result, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result)
+            }
+        });
+    })
+}
+
+exports.pool = pool;
